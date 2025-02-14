50c8892403c93daec46a36f193ea5169639e7910
kaaado <yacineyoyoker@gmail.com>
2025-02-14 10:50:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function App() {  
+  return (
+    <main >
+        <Header/>
+        <div className="pt-16">
+          <Outlet/>
+        </div>
+        <Footer/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../assets/logo.png';
+import { NavLink,Link } from 'react-router-dom';
+
+function Header() {
+    const navigation =[
+        {
+            label:'TV Shows',
+            link:'tv'
+        },
+        {
+            label:'Movies',
+            link:'movie'
+        }
+    ]
+  return (
+    <header className='fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-70'>
+        <div className='container mx-auto px-2 flex items-center h-full'>
+            <div>
+                <Link to=''>
+                    <img  src={logo} alt="Logo" width={120} />
+                </Link>
+            </div>
+            <nav className='flex items-center gap-2 ml-5 '>
+                {
+                    navigation.map((nav,index)=>{
+                    return(
+                        <div className='hover:text-neutral-50'>
+                            <NavLink to={nav.link}>{nav.label}</NavLink>
+                        </div>
+                        
+                    )})
+                }
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
